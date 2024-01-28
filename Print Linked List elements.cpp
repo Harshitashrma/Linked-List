4cac96adaa56f007ff939be17e2f2f3461116ad6
Harshita Sharma <155089213+Harshitashrma@users.noreply.github.com>
2024-01-28 19:26:52 +0530

    Create Print Linked List elements.cpp
    Given a linked list. Print all the elements of the linked list.
    
    Note :- End of Line is handled by driver code itself. You just have to end with a single space.

---
+++ Print Linked List elements.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+    public:
+    void display(Node *head)
+    {
+      //your code goes here
+      Node * temp=head;
+      while(temp!=NULL)
+      {
+          cout<<temp->data<<" ";
+          temp=temp->next;
+      }
+    }
+};
